c4bec35a30a72d9e3e76ece941479a9d6a12b87d
claudiosena79 <claudiosena79@gmail.com>
2023-09-25 17:08:50 -0300

    Classe final com tipos de Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 43;
+        short ano = 2023;
+        int cep = 21070333; // se valor iniciar com 0(0 a esquerda) utilize String
+        long cpf = 98765432109L; // se valor iniciar com 0(0 a esquerda) utilize String
+        float PI = 3.14F;
+        double salario = 12345.67;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;// uso do cast para mudar o tipo de variavel
+        System.out.println(numeroCurto);
+        numeroCurto = 2;
+        System.out.println(numeroCurto);
+
     }
 }
