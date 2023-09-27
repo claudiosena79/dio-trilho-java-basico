c71d0a3e2bdc99936ed3ac5e752b859d25eb41c1
claudiosena79 <claudiosena79@gmail.com>
2023-09-27 15:19:18 -0300

    Termino aula de operadores-Impressão de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,20 +2,30 @@ public class TiposVariaveis {
 
     public static void main(String[] args) throws Exception {
         byte idade = 43;
+        System.out.println(idade);
+
         short ano = 2023;
+        System.out.println(ano);
+
         int cep = 21070333; // se valor iniciar com 0(0 a esquerda) utilize String
+        System.out.println(cep);
+
         long cpf = 98765432109L; // se valor iniciar com 0(0 a esquerda) utilize String
-        // float PI = 3.14F;
+        System.out.println(cpf);
+
         double salario = 12345.67;
+        System.out.println(salario);
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;// uso do cast para mudar o tipo de variavel
+        System.out.println(numeroCurto2);
         System.out.println(numeroCurto);
         numeroCurto = 2;
         System.out.println(numeroCurto);
 
         final double PI = 3.14;
+        System.out.println(PI);
 
     }
 }
