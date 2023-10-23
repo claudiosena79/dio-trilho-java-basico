dbfca7b29f709a3575c354a2821329b7f1d5bc65
claudiosena79 <claudiosena79@gmail.com>
2023-10-23 16:04:04 -0300

    versão com trtamento de exceção

--- tipos-variaveis/src/SobreMim.java
+++ tipos-variaveis/src/SobreMim.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class SobreMim {
@@ -8,25 +9,30 @@ public class SobreMim {
 
         Scanner leitor = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome: ");
-        String nome = leitor.nextLine();
+        try {
+            System.out.println("Digite seu nome: ");
+            String nome = leitor.nextLine();
 
-        System.out.println("Digite seu sobrenome: ");
-        String sobrenome = leitor.nextLine();
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = leitor.nextLine();
 
-        System.out.println("Digite sua idade: ");
-        int idade = leitor.nextInt();
+            System.out.println("Digite sua idade: ");
+            int idade = leitor.nextInt();
 
-        System.out.println("Digite sua altura: ");
-        double altura = leitor.nextDouble();
+            System.out.println("Digite sua altura: ");
+            double altura = leitor.nextDouble();
 
-        System.out.println("Digite su peso: ");
-        double peso = leitor.nextDouble();
+            System.out.println("Digite su peso: ");
+            double peso = leitor.nextDouble();
 
-        System.out.println("O meu nome é " + nome + " " + sobrenome);
-        System.out.println("Tenho " + idade + "anos de idade");
-        System.out.println("Tenho " + altura + " cm de altura");
-        System.out.println("E peso " + peso + " quilos.");
+            System.out.println("O meu nome é " + nome + " " + sobrenome);
+            System.out.println("Tenho " + idade + "anos de idade");
+            System.out.println("Tenho " + altura + " cm de altura");
+            System.out.println("E peso " + peso + " quilos.");
+
+        } catch (InputMismatchException e) {
+            System.out.println("Os campos IDADE e ALTURA devem ser numéricos com as casas decimais separadas por ponto");
+        
 
         leitor.close();
 
